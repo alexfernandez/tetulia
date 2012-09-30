8acf3540d6df645ed7a20baf8f500f9c9c213790
huy <lehuy20@gmail.com>
2012-09-30 09:13:00 +0200

    a bit of refactor

--- TreeNode.java
+++ TreeNode.java
@@ -5,8 +5,25 @@ import static java.lang.System.*;
 public class TreeNode {
 
   public TreeNode parent;
+
   TreeNode findFirstCommonAncestor(TreeNode other) {
     return TreeNodeHelper.findFirstCommonAncestor(this, other);
+  }
+
+  int depth() {
+    int result = 0;
+    for(TreeNode node = this; node != null; node = node.parent)
+      result++;
+
+    return result;
+  }
+
+  TreeNode ancestor(int distance) {
+    TreeNode node = this;
+    for(int i = distance; i >0 && node != null; i--)
+      node = node.parent;
+
+    return node;
   }
   
 }
--- TreeNodeHelper.java
+++ TreeNodeHelper.java
@@ -1,48 +1,24 @@
 public class TreeNodeHelper {
 	
-	public static TreeNode findFirstCommonAncestor(final TreeNode _one, final TreeNode _two) {
+	public static TreeNode findFirstCommonAncestor(final TreeNode _one, final TreeNode _other) {
+		TreeNode commonAncestor = null, one = _one, other = _other;
 
 		// first I'm going to calculate the deep of each node (2n)
-		int oneDeep = 1,
-			twoDeep = 1;
-
-		TreeNode commonAncestor = null,
-				 one = _one, 
-				 two = _two;
-
-		while (one.parent != null) {
-			one = one.parent;
-			oneDeep++;
-		}
-
-		while (two.parent != null) {
-			two = two.parent;
-			twoDeep++;
-		}
-
-		one = _one;
-		two = _two;
-
-		// now I'm going to calculate the common ancestor (n)
+		int depthDiff = one.depth() - other.depth();
 
 		// first I don't need to check equality
-		while (oneDeep > twoDeep) {
-			oneDeep--;
-			one = one.parent;
-		}
-
-		while (twoDeep > oneDeep) {
-			twoDeep--;
-			two = two.parent;
-		}
+		if (depthDiff > 0) 
+                  one = one.ancestor(depthDiff);
+                else
+                  other = other.ancestor(-depthDiff);
 
 		// now I need to compare the equality
 		while (commonAncestor == null) {
-			if (one == two) {
+			if (one == other) {
 				commonAncestor = one;
 			} else {
 				one = one.parent;
-				two = two.parent;
+				other = other.parent;
 			}
 		}
 
